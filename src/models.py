0fa41efbaea874576a96b888727c8216a07b8087
wiz <yfbenkhalifa@gmail.com>
2023-05-15 17:26:45 +0200

    Update: Stable Model
    Stable Model with 0.18 Train Loss, 0.23 Test loss,  0.92 Train acc and 0.93 test acc

--- src/models.py
+++ src/models.py
@@ -148,17 +148,36 @@ class Multiclass(nn.Module):
         x = self.output(x)
         return x
 
+class BlobModel(nn.Module):
+    def __init__(self, input_features, output_features, hidden_units=8):
+        """Initializes all required hyperparameters for a multi-class classification model.
+
+        Args:
+            input_features (int): Number of input features to the model.
+            out_features (int): Number of output features of the model
+              (how many classes there are).
+            hidden_units (int): Number of hidden units between layers, default 8.
+        """
+        super().__init__()
+        self.linear_layer_stack = nn.Sequential(
+            nn.Linear(in_features=input_features, out_features=hidden_units),
+            # nn.ReLU(), # <- does our dataset require non-linear layers? (try uncommenting and see if the results change)
+            nn.Linear(in_features=hidden_units, out_features=hidden_units),
+            # nn.ReLU(), # <- does our dataset require non-linear layers? (try uncommenting and see if the results change)
+            nn.Linear(in_features=hidden_units, out_features=output_features), # how many classes are there?
+        )
+    
+    def forward(self, x):
+        return self.linear_layer_stack(x)
+    
 class PCDModel_1(torch.nn.Module):
-    def __init__(self, input_shape, output_shape):
+    def __init__(self, inputFeatures, outputFeatures):
         super().__init__()
         self.linearBlock = nn.Sequential(
-            nn.Linear(in_features=input_shape, out_features=32),
-            nn.ReLU(),
+            nn.Linear(in_features=inputFeatures, out_features=32),
             nn.Linear(in_features=32, out_features=16),
-            nn.ReLU(),
             nn.Linear(in_features=16, out_features=8),
-            nn.ReLU(),
-            nn.Linear(in_features=8, out_features=output_shape),
+            nn.Linear(in_features=8, out_features=outputFeatures),
         )
         self.initWeights()
 
